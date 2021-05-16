9a89f91f8a51f1d7c95b5df48b6a61bb395f1c5c
sebi <gore.sebyx@yahoo.com>
2021-05-16 16:00:30 +0100

    fix web url

--- app/services/settings_service/web_url.rb
+++ app/services/settings_service/web_url.rb
@@ -1,14 +1,22 @@
+# frozen_string_literal: true
+
 module SettingsService
   class WebUrl
-    attr_reader :uri
-    def initialize
-      @uri = URI.parse(Rails.application.secrets.web_url)
-    end
-
     def full_host
-      return nil if uri.host.blank?
+      return nil if uri&.host.blank?
 
       "https://" + uri.host + (Rails.env.production? ? "" : ":#{uri.port}")
     end
+
+    private
+      def uri
+        return @uri if defined? @uri
+        return @uri = nil if env_web_url.blank?
+        @uri = URI.parse(env_web_url)
+      end
+
+      def env_web_url
+        @env_web_url ||= Rails.application.secrets.web_url
+      end
   end
 end
