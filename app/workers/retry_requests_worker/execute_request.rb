5dafbc995dc467c8d66652c2daf17980ec3d29c6
sebi <gore.sebyx@yahoo.com>
2021-06-02 06:39:38 +0100

    update handle_no_result

--- app/workers/retry_requests_worker/execute_request.rb
+++ app/workers/retry_requests_worker/execute_request.rb
@@ -12,16 +12,21 @@ module RetryRequestsWorker
       retry_request.update!(status: :running)
 
       result = retry_request.execute
-      return if result.blank?
+      return handle_no_result if result.blank?
       return handle_success if result.valid?
       return handle_too_many_retries if retry_request.max_retries?
 
-      handle_retry(result)
+      handle_retry_result(result)
     end
 
     private
       def handle_success
         retry_request.update!(status: :completed)
+      end
+
+      def handle_no_result
+        retry_request.status = :failed
+        retry_request.save!
       end
 
       def handle_too_many_retries
@@ -30,7 +35,7 @@ module RetryRequestsWorker
         retry_request.save!
       end
 
-      def handle_retry(result)
+      def handle_retry_result(result)
         retry_request.increment(:retries)
         retry_request.peer_ids = result.failed_peer_ids
         retry_request.save!
