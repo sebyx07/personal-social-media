473a2a24fc93b8f82b631dff6a64648cecef5061
sebi <gore.sebyx@yahoo.com>
2021-06-07 06:15:57 +0100

    fix indexes migration

--- db/migrate/20210607050125_fix_indexes_7_june_2021.rb
+++ db/migrate/20210607050125_fix_indexes_7_june_2021.rb
@@ -2,9 +2,9 @@
 
 class FixIndexes7June2021 < ActiveRecord::Migration[6.1]
   def change
-    remove_index :psm_cdn_files, name: "index_psm_cdn_files_on_psm_file_variant_id", column: :psm_file_variant_id
-    remove_index :psm_file_permanents, name: "index_psm_file_permanents_on_psm_file_variant_id", column: :psm_file_variant_id
-    remove_index :psm_file_variants, name: "index_psm_file_variants_on_psm_file_id", column: :psm_file_id
-    remove_index :psm_permanent_files, name: "index_psm_permanent_files_on_psm_file_variant_id", column: :psm_file_variant_id
+    remove_index :psm_cdn_files, :psm_file_variant_id if index_exists? :psm_cdn_files, :psm_file_variant_id
+    remove_index :psm_file_permanents, :psm_file_variant_id if index_exists? :psm_file_permanents, :psm_file_variant_id
+    remove_index :psm_file_variants, :psm_file_id if index_exists? :psm_file_variants, :psm_file_id
+    remove_index :psm_permanent_files, :psm_file_variant_id if index_exists? :psm_permanent_files, :psm_file_variant_id
   end
 end
