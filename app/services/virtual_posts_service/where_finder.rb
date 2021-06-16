fb3e564192926540e387e0c46ae3f80713766569
sebi <gore.sebyx@yahoo.com>
2021-06-16 16:26:17 +0100

    finish includes

--- app/services/virtual_posts_service/where_finder.rb
+++ app/services/virtual_posts_service/where_finder.rb
@@ -3,8 +3,8 @@
 module VirtualPostsService
   class WhereFinder
     class Error < StandardError; end
-    PRELOAD_ASSOCIATIONS_LOCALLY = [:remote_post, :reaction_counters, :cache_reactions, :comment_counter]
-    PRELOAD_ASSOCIATIONS_EXTERNALLY = [:peer, :cache_reactions]
+    PRELOAD_ASSOCIATIONS_LOCALLY = [:remote_post, :reaction_counters, :cache_reactions, :comment_counter, cache_comments: :peer, latest_comments: :peer]
+    PRELOAD_ASSOCIATIONS_EXTERNALLY = [:peer, :cache_reactions, cache_comments: :peer]
     DEFAULT_LIMIT = 15
 
     attr_reader :pagination_params, :peer_id, :post_type, :show_from_feed_only
--- config/initializers/bullet_whitelist.rb
+++ config/initializers/bullet_whitelist.rb
@@ -3,4 +3,6 @@
 return unless DeveloperService::IsEnabled.is_enabled?
 
 Bullet.add_whitelist type: :unused_eager_loading, class_name: "RemotePost", association: :cache_reactions
+Bullet.add_whitelist type: :unused_eager_loading, class_name: "RemotePost", association: :cache_comments
+
 Bullet.add_whitelist type: :unused_eager_loading, class_name: "Post", association: :cache_reactions
