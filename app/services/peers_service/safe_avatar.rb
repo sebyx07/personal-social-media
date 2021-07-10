efedbb5724d34a5c929e95e1c3190f651a8dd719
sebi <gore.sebyx@yahoo.com>
2021-07-10 13:14:23 +0100

    improve safe avatar

--- app/services/peers_service/safe_avatar.rb
+++ app/services/peers_service/safe_avatar.rb
@@ -10,7 +10,7 @@ module PeersService
 
     def url(size:)
       @size = size
-      if peer.email_hexdigest.present? || peer.email.present?
+      if can_render_gravatar?
         return gravatar
       end
       blank_avatar
@@ -22,7 +22,18 @@ module PeersService
       end
 
       def gravatar
-        PeersService::Gravatar.new(email_hexdigest: peer.email_hexdigest).url(size: size)
+        gravatar_options = {}
+        if peer.respond_to?(:email_hexdigest)
+          gravatar_options[:email_hexdigest] = peer.email_hexdigest
+        elsif peer.respond_to?(:email)
+          gravatar_options[:email] = peer.email
+        end
+
+        PeersService::Gravatar.new(**gravatar_options).url(size: size)
+      end
+
+      def can_render_gravatar?
+        peer.try(:email_hexdigest).present? || peer.try(:email).present?
       end
   end
 end
