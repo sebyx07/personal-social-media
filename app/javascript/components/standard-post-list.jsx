9d8a813d63519c156470b36b9004bd3b93a1ad45
sebi <gore.sebyx@yahoo.com>
2021-07-16 05:08:16 +0100

    eslint

--- app/javascript/components/standard-post-list.jsx
+++ app/javascript/components/standard-post-list.jsx
@@ -1,7 +1,7 @@
+import {StrictMode} from 'react';
 import InfiniteList from './util/infinite-list';
 import StandardPost from './standard-posts/standard-post';
 import useInfiniteResource from './util/infinite-list/use-infinite-resource';
-import {StrictMode} from 'react';
 
 export default function StandardPostList({peerId, showFromFeedOnly}) {
   const query = {peerId, post_type: 'standard', showFromFeedOnly};
