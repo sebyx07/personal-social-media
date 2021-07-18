42d5d9df265e61448abc97c950f3a211af6af3d2
sebi <gore.sebyx@yahoo.com>
2021-07-18 23:10:45 +0100

    add compute file sha

---
+++ app/services/psm_files_service/utils/compute_sha.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module PsmFilesService
+  module Utils
+    class ComputeSha
+      attr_reader :file, :buffer_size
+      def initialize(file, buffer_size: 2.megabytes)
+        @file = file
+        @buffer_size = buffer_size
+      end
+
+      def call
+        ReadFileAsStream.new(file, buffer_size).read do |input|
+          digest.update(input)
+        end
+
+        digest.hexdigest
+      end
+
+      def digest
+        @digest ||= Digest::SHA256.new
+      end
+    end
+  end
+end
---
+++ spec/services/psm_files_service/utils/compute_sha_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe PsmFilesService::Utils::ComputeSha do
+  let(:file) { File.open(Rails.root.join("spec/support/resources/picture.jpg"), "rb") }
+  let(:read_1kb) do
+    described_class.new(file, buffer_size: 1.kilobyte).call
+  end
+
+  subject do
+    described_class.new(file).call
+  end
+
+  it "computes the sha of file" do
+    expect(subject).to eq("28303e77cdd92ad08e2d746c76f0b901e33fe453015672afb4d9f8e9f5eb252f")
+    expect(subject).to eq(read_1kb)
+  end
+end
