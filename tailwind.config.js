6ec71b721526f488ae3c61871d9d4c4d4f3d5ed7
sebi <gore.sebyx@yahoo.com>
2021-08-29 07:06:46 +0100

    add theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,12 @@ module.exports = {
   // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        'cyber-1': '#031b20',
+        'cyber-1-transparent': 'rgba(3,27,32,0.90)',
+        'cyber-2': '#00f8f8',
+        'cyber-2-light': 'rgb(126, 252, 246)',
+      },
       cursor: {
         'zoom-in': 'zoom-in',
       },
